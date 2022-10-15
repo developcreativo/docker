4ca21dc9183b00ba4ea9cb5249842e35108c7222
Kelly Salazar <developmentcreativo@gmail.com>
2022-10-15 12:39:37 +0000

    cambios

---
+++ workspace/topdf.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+docker run --rm -i vladgolubev/libreoffice-pdf-cli
+docker run --rm -i macgyvertechnology/face-comparison-model:2
+docker run --rm -i vladgolubev/libreoffice-pdf-cli:latest
